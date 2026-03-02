f116fa9690ec8b3f22abc03c2de25f5df6b2461e
Khakimzyanov.Ilya <161026khakimzyanov.ilya@users.no-reply.gitverse.ru>
2026-03-03 00:00:32 +0500

    test model_preparation

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,8 +1,8 @@
 import sys
+from pathlib import Path
 import pandas as pd
 import pickle
 from sklearn.linear_model import LogisticRegression
-import os
 
 # Взятие рандома из .sh
 if len(sys.argv) > 1:
@@ -10,11 +10,19 @@ if len(sys.argv) > 1:
 else:
     random_id = 123
 
+# Расположение
+base_path = Path(__file__).parent
+train_folder_name = 'train'
+train_folder_path = base_path / train_folder_name
+train_file = train_folder_path / 'train_preprocessed.csv'
+model_folder_path = base_path / 'models'
+model_file_name = 'model.pkl'
+
 # Загрузка предобработанных данных
-if not os.path.exists("train/train_preprocessed.csv"):
+if not train_file.exists():
     raise FileNotFoundError("Предобработанные данные не найдены.")
 
-train_df = pd.read_csv("train/train_preprocessed.csv")
+train_df = pd.read_csv(train_file)
 
 X_train = train_df.drop('target', axis=1)
 y_train = train_df['target']
@@ -24,5 +32,6 @@ model = LogisticRegression(random_state=random_id)
 model.fit(X_train, y_train)
 
 # Сохранение модели в файл
-with open('model.pkl', 'wb') as f:
+model_folder_path.mkdir(exist_ok=True)
+with open(model_folder_path / model_file_name, 'wb') as f:
     pickle.dump(model, f)
